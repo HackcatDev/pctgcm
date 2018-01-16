c6dade562251ab0e2d7a9028adb5a6ea0451bccf
HackcatDev <35487139+HackcatDev@users.noreply.github.com>
2018-01-16 13:45:56 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Timers;
+using Newtonsoft.Json;
+
+namespace PCTGCM
+{
+    public class Program
+    {
+        
+        public static void Main()
+        {
+
+            GameProcessManager gpm = new GameProcessManager();
+            gpm.Init();
+            Console.ReadKey();
+
+        }
+
+    }
+}
